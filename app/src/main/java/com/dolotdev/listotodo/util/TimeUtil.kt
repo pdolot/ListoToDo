7c0c219019d22ba60e153c4212cc36b3cb0a194c
pdolecinski <patryk.dolecinski@gmail.com>
2020-04-23 16:31:11 +0200

    Autoformat

--- app/src/main/java/com/dolotdev/listotodo/util/TimeUtil.kt
+++ app/src/main/java/com/dolotdev/listotodo/util/TimeUtil.kt
@@ -5,10 +5,10 @@ object TimeUtil {
     fun getElapsedTime(startTime: Long): String {
         val timeDiff = (System.currentTimeMillis() - startTime) / 1000
 
-        var days = timeDiff / (60 * 60 * 24)
-        var hours = (timeDiff % (60 * 60 * 24)) / (60 * 60)
-        var minutes = (timeDiff % (60 * 60)) / 60
-        var seconds = timeDiff % 60
+        val days = timeDiff / (60 * 60 * 24)
+        val hours = (timeDiff % (60 * 60 * 24)) / (60 * 60)
+        val minutes = (timeDiff % (60 * 60)) / 60
+        val seconds = timeDiff % 60
 
         return "${days}d ${hours}h ${minutes}m ${seconds}s"
     }
